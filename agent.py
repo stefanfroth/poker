fix exploding gradient problem and play v1 v v2

agent.py
@@ -2,14 +2,14 @@ import itertools
 import pandas as pd
 import sqlalchemy as sqa
 import numpy as np
-from tensorflow.keras import models, layers, metrics, optimizers
+from tensorflow.keras import models, layers, metrics, optimizers, utils
 from tensorflow.keras.models import model_from_json, Sequential
-from tensorflow.keras.layers import Input, Dense, Activation, Conv2D, MaxPooling2D, Flatten, Dropout
+from tensorflow.keras.layers import Input, Dense, Activation, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
 from tensorflow.keras import backend as K
 import tensorflow as tf
 import datetime
 
-DB = f'postgres://localhost/{"poker"}'
+DB = f'postgres://localhost/poker'
 ENGINE = sqa.create_engine(DB)
 
 max_features = 53
@@ -46,6 +46,7 @@ class Agent:
         # Card embeddings
         card_input = Input(shape=(7,))
         card_output = layers.Embedding(self.max_features, self.vector_size, input_length=self.max_len)(card_input)
+        #card_output = layers.BatchNormalization()(card_output)
         card_output = layers.Flatten()(card_output)
 
         # Other information
@@ -53,8 +54,12 @@ class Agent:
 
         #Merge and add dense layer
         merge_layer = layers.concatenate([state_input, card_output])
-        x = layers.Dense(64, activation='relu')(merge_layer)
+        x = layers.BatchNormalization()(merge_layer)
+        x = layers.Dense(64, activation='relu')(x)
+        #x = layers.Dense(64, activation='relu')(merge_layer)
+        x = layers.BatchNormalization()(x)
         x = layers.Dense(32, activation='relu')(x)
+        x = layers.BatchNormalization()(x)
         main_output = layers.Dense(3, activation='softmax')(x)
 
         # Define model with two inputs
@@ -64,30 +69,30 @@ class Agent:
         #    action_prob_ph = self.model.output
         action_prob_ph = self.model.output#K.sum(self.model.output)
         #print(f'The probabilities for the three actions are {action_prob_ph}')
-        self.action_onehot_ph = K.placeholder(shape=(None, 3),
+        action_onehot_ph = K.placeholder(shape=(None, 3),
                                                  name="action_onehot")
         discount_reward_ph = K.placeholder(shape=(None,),
                                                     name="discount_reward")
 
-        action_prob = K.sum(action_prob_ph * self.action_onehot_ph, axis=1)
+        action_prob = K.sum(action_prob_ph * action_onehot_ph, axis=1)
         log_action_prob = K.log(action_prob)
 
         loss = - log_action_prob * discount_reward_ph
         loss = K.mean(loss)
 
-        adam = optimizers.Adam()
+        self.adam = optimizers.Adam(clipvalue=0.5)
 
-        updates = adam.get_updates(params=self.model.trainable_weights,
+        updates = self.adam.get_updates(params=self.model.trainable_weights,
                                    #constraints=[],
                                    loss=loss)
 
         self.train_fn = K.function(inputs=[self.model.input[0],
                                             self.model.input[1],
-                                           self.action_onehot_ph,
+                                           action_onehot_ph,
                                            discount_reward_ph],
-                                   outputs=[action_prob], updates=updates)
+                                   outputs=[loss, action_onehot_ph+1, action_prob_ph, action_prob, log_action_prob, discount_reward_ph+1], updates=updates)
 
-        self.model.compile(optimizer=adam, loss='categorical_crossentropy')
+        self.model.compile(optimizer=self.adam, loss='categorical_crossentropy')
 
         # for i in tf.get_default_graph().get_operations():
         #     if i.name == 'action_onehot' or i.name == 'discount_reward':
@@ -96,11 +101,12 @@ class Agent:
         return self.model
 
 
-    def read_data(self):
+    def read_data(self, table_name):
         '''
         The method read_model reads the data saved into the postgres database.
         '''
-        self.input = pd.read_sql('results', con=ENGINE)
+        self.input = pd.read_sql(table_name, con=ENGINE)
+        #print(f'The data read in is {self.input}')
 
         # for i in tf.get_default_graph().get_operations():
         #     if i.name == 'action_onehot' or i.name == 'discount_reward':
@@ -146,17 +152,27 @@ class Agent:
                 print(i.name)
 
 #        print(f'The operations are: {tf.get_default_graph().get_operations()}')
-        b = np.zeros((actions.shape[0], 3))
-        b[np.arange(actions.shape[0]), actions] = 1
+        #b = np.zeros((actions.shape[0], 3))
+        #b[np.arange(actions.shape[0]), actions] = 1
+
+        action_onehot = utils.to_categorical(actions, num_classes=3)
+        rewards = np.float32(rewards)
+        cards = np.float32(cards)
+        states = np.float32(states)
 
         n = actions.shape[0]
         assert rewards.shape[0] == n
         assert cards.shape[0] == n
         assert states.shape[0] == n
-        assert b.shape[1] == 3
+        assert action_onehot.shape[1] == 3
         assert cards.shape[1] == 7
         assert states.shape[1] == 13
-        self.train_fn(inputs=[cards, states, b, rewards])
+        print(f'''The states are {states}, the cards are {cards}, the actions are {action_onehot} and the rewards are {rewards}.
+        #We are going to train the model using these inputs.''')
+        loss = self.train_fn(inputs=[cards, states, action_onehot, rewards])
+        #log_loss = np.log(loss[1])
+        #log_loss
+        return loss[0]
 
 
     def train(self, X, y, epochs):
@@ -167,7 +183,9 @@ class Agent:
     def save(self):
         date_string = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
         with open(f"./weights/weights_{date_string}.json", "w") as json_file:
+        #with open('./weights/weights_test2.json', 'w') as json_file:
             json_file.write(self.model.to_json())
+        #self.model.save_weights('./weights/weights_test2.h5')
         self.model.save_weights(f'./weights/weights_{date_string}.h5')
 
     def load(self, date_string):
@@ -175,12 +193,42 @@ class Agent:
         #     if i.name == 'action_onehot' or i.name == 'discount_reward':
         #         print(i.name + 'load_1')
         #K.clear_session()
+        #self.build_model()
         with open(f'./weights/weights_{date_string}.json', 'r') as f:
             json = f.read()
         self.model = model_from_json(json)
         self.model.load_weights(f'./weights/weights_{date_string}.h5')
-        # 'adam' als string nimmt allerdings die Standardeinstellungen von adam
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy')
+        # 'self.adam' als string nimmt allerdings die Standardeinstellungen von self.adam
+        #self.model.compile(optimizer='adam', loss='categorical_crossentropy')
+        #self.build_model()
+
+        #    action_prob_ph = self.model.output
+        action_prob_ph = self.model.output#K.sum(self.model.output)
+        #print(f'The probabilities for the three actions are {action_prob_ph}')
+        action_onehot_ph = K.placeholder(shape=(None, 3),
+                                                 name="action_onehot")
+        discount_reward_ph = K.placeholder(shape=(None,),
+                                                    name="discount_reward")
+
+        action_prob = K.sum(action_prob_ph * action_onehot_ph, axis=1)
+        log_action_prob = K.log(action_prob)
+
+        loss = - log_action_prob * discount_reward_ph
+        loss = K.mean(loss)
+
+        self.adam = optimizers.Adam(clipvalue=0.5)
+
+        updates = self.adam.get_updates(params=self.model.trainable_weights,
+                                   #constraints=[],
+                                   loss=loss)
+
+        self.train_fn = K.function(inputs=[self.model.input[0],
+                                            self.model.input[1],
+                                           action_onehot_ph,
+                                           discount_reward_ph],
+                                   outputs=[loss, action_onehot_ph+1, action_prob_ph, action_prob, log_action_prob, discount_reward_ph+1], updates=updates)
+
+        self.model.compile(optimizer=self.adam, loss='categorical_crossentropy')
 
         # for i in tf.get_default_graph().get_operations():
         #     if i.name == 'action_onehot' or i.name == 'discount_reward':

play.py
@@ -5,7 +5,6 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
 from poker import Game
 from agent import Agent
-import sqlalchemy as sqa
 import time
 start_time = time.time()
 
@@ -16,40 +15,38 @@ PLAYERS = 6
 BLIND = 50
 STACK = 10000
 LIMIT = 100
-GAMES = 50000
+GAMES = 25000
 AGENT = Agent()
+AGENT.load('2019-08-16-13:59')
+print(f'The models weights before playing are: {AGENT.model.get_weights()}')
 
 
 #AGENT.build_model()
 # #print(AGENT.model.layers[1].get_weights())
 #
-# g = Game(PLAYERS, BLIND, STACK, AGENT, LIMIT)
+g = Game(PLAYERS, BLIND, STACK, AGENT, LIMIT)
 #
 #
-# for i in range(GAMES):
-#     g.play_one_complete_game()
+for i in range(GAMES):
+    g.play_one_complete_game()
 #
 # print(f'{GAMES} games were played!')
 #
-# AGENT.save()
-#
+#AGENT.save()
+
+#AGENT.read_data('results')
+
+#AGENT.create_embedding_input()
+#AGENT.create_state_input()
+
+#for i in range(10):
+#    AGENT.train_model(AGENT.input_card_embedding, AGENT.input_state, np.array(AGENT.input['action']), np.array(AGENT.input['reward']))
+
+#print(f'The models weights after playing are: {AGENT.model.get_weights()}')
 # print('''The weights were saved! Now the model will be retrained on the generated
 #         data''')
+#time.sleep(60)
+#AGENT.save()
 
-AGENT.load('2019-08-14-00:24')
-#print('Loaded successfully')
-AGENT.read_data()
-#print('Read the data successfully')
-AGENT.create_embedding_input()
-#print(f'Created embedding input {AGENT.input_card_embedding}')
-AGENT.create_state_input()
-#print(f'Created state input {AGENT.input_state}')
-#print(f'The actions are {AGENT.input['action']}')
-#print(f'The rewards are {AGENT.input['reward']}')
-#print(f'The model operations are: {AGENT.model.get_operations()}')
-AGENT.train_model(AGENT.input_card_embedding, AGENT.input_state, np.array(AGENT.input['action']), np.array(AGENT.input['reward']))
-
-AGENT.save()
-
-print('''The model has been retrained and the new weights were saved.
+print('''We have generated data points.
 This took {} to run'''.format(time.time() - start_time))

poker_verbose.py
@@ -317,7 +317,7 @@ class Player():
 
     def chose_action(self, state):
         self.action = np.squeeze(np.random.choice(3, 1, p=np.squeeze(self.agent.model.predict(state))))
-       #print('I chose the action {}'.format(self.action))
+        print('I chose the action {}'.format(self.action))
         return self.action
 
 
@@ -334,7 +334,7 @@ class Player():
         '''
         self.last_actions.append(0)
         self.active = 0
-        #print(f'Player {self.name}: My cards are sh..! I fold!')
+        print(f'Player {self.name}: My cards are sh..! I fold!')
 
 
 #    def check(self):
@@ -353,7 +353,7 @@ class Player():
         '''
         self.last_actions.append(1)
         self.own_bet = highest_bet
-       #print(f'Player {self.name}: I think I can win this game. I am calling!')
+        print(f'Player {self.name}: I think I can win this game. I am calling!')
 
 
     def raise_bet(self, highest_bet, limit):
@@ -369,15 +369,15 @@ class Player():
         The function do determines the action of the player when it is his turn
         to bet.
         '''
-        #print(f'My own bet is {self.own_bet} and the original stack size is {self.stack_original}')
+        print(f'My own bet is {self.own_bet} and the original stack size is {self.stack_original}')
         if blind == 'small': # was, wenn er nicht mehr genug Geld für den small blind hat?
             self.own_bet = self.blind
             self.last_actions.append(2)
-           #print(f'Player {self.name}: I have bet the small blind of ${self.blind}!')
+            print(f'Player {self.name}: I have bet the small blind of ${self.blind}!')
         elif blind == 'big': # was, wenn er nicht mehr genug Geld für den big blind hat?
             self.own_bet = self.blind * 2
             self.last_actions.append(2)
-           #print(f'Player {self.name}: I have bet the big blind of ${self.blind*2}!')
+            print(f'Player {self.name}: I have bet the big blind of ${self.blind*2}!')
         elif self.agent:
             if highest_bet == self.stack_original:
                 self.call(highest_bet)
@@ -387,7 +387,7 @@ class Player():
                 # check that it does not fold if player.own_bet == highest_bet
                 #else:
                 #    action = random.choice(['call', 'raise'])
-                #print(f'Player {self.name} chose action {action}.')
+                print(f'Player {self.name} chose action {action}.')
                 if action == 0:
                     self.fold()
                 #elif action == 'check':
@@ -396,8 +396,8 @@ class Player():
                     self.call(highest_bet)
                 else:
                     self.raise_bet(highest_bet, limit)
-                #print(f'''Player {self.name}: I have {self.actions[self.last_actions[-1]]}ed
-                #and am betting ${self.own_bet}!''')
+                print(f'''Player {self.name}: I have {self.actions[self.last_actions[-1]]}ed
+                and am betting ${self.own_bet}!''')
         else:
             self.fold()
 
@@ -422,7 +422,7 @@ class Player():
 
         ranked = sorted(hand_values_rank, key=lambda x: (x[2], x[3]), reverse=False)
        #print([(card.face, card.suit) for card in possible_hands[ranked[0][0]]])
-       #print(f'The best hand of player {self.name} is a {ranked[0][1]}')
+        print(f'The best hand of player {self.name} is a {ranked[0][1]}')
 
         #if ranked[0][3][0] in [ranked[i][3][0] for i in range(len(hand_values_rank))]:
         #    print('This outcomes exists more than once')
@@ -546,7 +546,7 @@ class Game:
                 highest += 1
             if self.players[position].active == 0:
                 self.active_players -= 1
-            #print(f'highest is {highest} and active players are {self.active_players}')
+            print(f'highest is {highest} and active players are {self.active_players}')
 
         if highest == self.active_players and self.active_players >= 1:
 
@@ -571,12 +571,12 @@ class Game:
                 active += 1
                 ap = player
         if active == 1:
-           #print('The game has ended')
+            print('The game has ended')
             self.winner = ap
             self.active_players = 1
             return 0
         if active == 0:
-           #print('Something went wrong')
+            print('Something went wrong')
             return 1
         else:
 #            print('The game has not ended')
@@ -621,8 +621,8 @@ class Game:
             card2 = Card(self.deck.pop())
             #print(card2, card2.face, card2.suit)
             player.hand = [card1, card2]
-           #print(f"{player.name}'s hand is {[(card.face, card.suit) for card in player.hand]} ")
-            #assert len(player.hand) == 2
+            print(f"{player.name}'s hand is {[(card.face, card.suit) for card in player.hand]} ")
+            assert len(player.hand) == 2
 
 
     def iterative_play(self):
@@ -641,7 +641,7 @@ class Game:
                 #print('entered for loop')
                 if self.players[player].own_bet <= self.highest_bet\
                 and self.players[player].active == 1:
-                   #print(f'The highest bet is {self.highest_bet}')
+                    print(f'The highest bet is {self.highest_bet}')
                     if self.highest_bet == self.stack:
                         self.players[player].do(self.highest_bet, self.limit, [[self.players[player].input_card_embedding], [self.players[player].input_state]])
                     else:
@@ -657,7 +657,7 @@ class Game:
                         # need to include some way to stop the game here
                         if self.check_end_of_game() == 0:
                             return 0
-                    #print(f'player {position+1} plays {self.players[position].own_bet}')
+                    print(f'player {position+1} plays {self.players[position].own_bet}')
             check_activity_round = self.check_activity_round()
 #            print(check_activity_round)
 #            print(not check_activity_round)
@@ -738,7 +738,7 @@ class Game:
         The function add_reward adds the reward of each player to the output
         data frame that is written to the database.
         '''
-        #print(self.output)
+        print(self.output)
         self.output['reward'] = self.output['player']
         for i in range(self.output.shape[0]):
             self.output.at[i, 'reward'] =\
@@ -752,7 +752,7 @@ class Game:
         '''
         self.output.to_sql('results', con=ENGINE, if_exists='append')
 
-        #print('Wrote the data into the database!')
+        print('Wrote the data into the database!')
 
         # reset the output_frame
         self.output = pd.DataFrame()
@@ -768,7 +768,7 @@ class Game:
         # action of the big blind player
         self.players[self.order[1]].do(self.highest_bet, self.limit, [], blind='big')
         self.highest_bet = self.players[self.order[1]].own_bet
-        #print(f'The highest bet after the big blind is {self.highest_bet}')
+        print(f'The highest bet after the big blind is {self.highest_bet}')
 
         # let the non-blind players take their turn
         for position, player in enumerate(self.order[2:]):
@@ -778,14 +778,14 @@ class Game:
             #self.write_data(player, position)
             else:
                 self.collect_data(player, position+2)
-                #print(f'The current state is described by {self.output.loc[[self.output.shape[0]-1]]} and the output shape is {self.output.shape[0]}')
+                print(f'The current state is described by {self.output.loc[[self.output.shape[0]-1]]} and the output shape is {self.output.shape[0]}')
                 self.players[player].create_embedding_input(self.output.loc[[self.output.shape[0]-1]])
                 self.players[player].create_state_input(self.output.loc[[self.output.shape[0]-1]])
 #                print(f'''The inputs for the action will be the cards {self.players[player].input_card_embedding} and the rest of the state {self.players[player].input_state }
 #                or combined {[[self.players[player].input_card_embedding], [self.players[player].input_state]]}''')
                 self.players[player].do(self.highest_bet, self.limit, [[self.players[player].input_card_embedding], [self.players[player].input_state]])
                 self.save_action(player)
-                #print(f'player {position+1} bet {self.players[position].own_bet}')
+                print(f'player {position+1} bet {self.players[position].own_bet}')
                 if self.players[player].own_bet > self.highest_bet:
                     self.highest_bet = self.players[player].own_bet
 
@@ -795,7 +795,7 @@ class Game:
         check_activity_round = self.check_activity_round()
         if check_activity_round:
              return 0
-             #print(f'''round {self.round} is over and {self.active_players}
+             print(f'''round {self.round} is over and {self.active_players}
             # players are still in the game!''')
 
         # play iterative_play
@@ -811,7 +811,7 @@ class Game:
         '''
         [self.flop.append(self.deck.pop()) for x in range(3)]
         [player.hand.append(Card(x)) for player in self.players for x in self.flop]
-        #print(f'The flop is: {self.flop}')
+        print(f'The flop is: {self.flop}')
         #for player in self.players:
         #    if player.active == 1:
         #        print('Player {} has the hand {}'\
@@ -835,7 +835,7 @@ class Game:
         '''
         self.flop.append(self.deck.pop())
         [player.hand.append(Card(self.flop[-1])) for player in self.players]
-       #print(f'The community cards after the turn are: {self.flop}')
+        print(f'The community cards after the turn are: {self.flop}')
 
 
     def deal_river(self):
@@ -844,7 +844,7 @@ class Game:
         '''
         self.flop.append(self.deck.pop())
         [player.hand.append(Card(self.flop[-1])) for player in self.players]
-       #print(f'The community cards after the river are: {self.flop}')
+        print(f'The community cards after the river are: {self.flop}')
 
 
     def pass_to_next_game(self):
@@ -861,15 +861,15 @@ class Game:
                     best_hands.append(player.evaluate_hand())
             winning_hand = sorted(best_hands, key=lambda x: (x[2], x[3]),\
                 reverse=False)[0]
-           #print(f'''Player {winning_hand[5]} wins with a {winning_hand[1]}.
-            #His hand is {winning_hand[4]}''')
+            print(f'''Player {winning_hand[5]} wins with a {winning_hand[1]}.
+            His hand is {winning_hand[4]}''')
             self.winner = winning_hand[5]
         else:
             for player in self.players:
                 if player.active == 1:
                     self.winner = player.name
-                    #print(f'''Player {self.winner} wins because everyone else dropped
-                    #out.''')
+                    print(f'''Player {self.winner} wins because everyone else dropped
+                    out.''')
 
         # distribute the pot
         self.pot = self.determine_pot_size()
@@ -878,8 +878,8 @@ class Game:
 
         for player in self.players:
             player.stack -= player.own_bet
-            #print(f"Player {player.name}'s stack after game {self.game_count} \
-            #is {player.stack}.")
+            print(f"Player {player.name}'s stack after game {self.game_count} \
+            is {player.stack}.")
             player.reward = player.stack - player.stack_old
             # update old stack for the next round
             player.stack_old = player.stack